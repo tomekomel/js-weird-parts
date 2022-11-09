29687a8eaeefb9a53d6687b57721faa0fcd46b75
Tomasz Omeljaniuk <tomekomel@gmail.com>
2022-11-09 16:50:23 +0100

    feat: comperison operators

--- operators/app.js
+++ operators/app.js
@@ -5,3 +5,17 @@ a = b = c;
 console.log(a);
 console.log(b);
 console.log(c);
+
+var a = 1 + '2';
+
+console.log(a);
+console.log(3 < 2 < 1);
+
+var p = 0;
+var k = false;
+
+if (p === k) {
+    console.log('They are equal!');
+} else {
+    console.log('Nope, they are not.');
+}
