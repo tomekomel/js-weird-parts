5d803e8b28f661c2b3807449c7738d46afd9fba2
Tomasz Omeljaniuk <tomekomel@gmail.com>
2022-11-11 15:51:39 +0100

    feat: functions are objects

---
+++ functions/app.js
@@ -0,0 +1,7 @@
+function greet() {
+    console.log('Greetings!');
+}
+
+greet.lanaguage = 'English';
+
+console.log(greet.lanaguage);
--- objects/app.js
+++ objects/app.js
@@ -13,3 +13,21 @@ person.address = new Object();
 person.address.street = "121 street";
 
 console.log(person);
+
+var anotherPerson = {
+    firstName: 'Joanna',
+    lastName: 'Johnson',
+    address: {
+        street: '1 av',
+        city: 'NY',
+    },
+};
+
+console.log(anotherPerson);
+
+function greet(person) {
+    console.log('Hello ' + person.firstName);
+}
+
+greet(person);
+greet({ firstName: 'Mark', lastName: 'Caven' });
