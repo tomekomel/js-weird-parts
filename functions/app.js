22118c22551f4395dfe17f7dd38a3a0f50ecb361
Tomasz Omeljaniuk <tomekomel@gmail.com>
2022-11-14 12:30:01 +0100

    feat: function statemants vs expressions

--- functions/app.js
+++ functions/app.js
@@ -5,3 +5,24 @@ function greet() {
 greet.lanaguage = 'English';
 
 console.log(greet.lanaguage);
+
+greet();
+
+function greet() {
+    console.log('hi');
+}
+
+var anonymousGreet = function() {
+    console.log('hi');
+}
+
+anonymousGreet();
+
+function log(a) {
+    a();
+}
+
+log(function () {
+    console.log('hi!');
+});
+
