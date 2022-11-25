7e02d93dba089929863ee537a14ad26787adf264
Tomasz Omeljaniuk <tomekomel@gmail.com>
2022-11-25 12:15:33 +0100

    feat: functional programming part 3

--- functional-programming/app.js
+++ functional-programming/app.js
@@ -41,3 +41,9 @@ var checkPastLimitSimplified = function (limiter) {
 // we need to preset one parameter
 var arr5 = mapEachElement(arr, checkPastLimit.bind(this, 2));
 console.log(arr5);
+
+var arr6 = _.map(arr, function (item) { return item * 3 });
+console.log(arr6);
+
+var arr7 = _.filter([1, 2, 3, 4, 5, 6, 7], function (item) { return item % 2 === 0 });
+console.log(arr7);
