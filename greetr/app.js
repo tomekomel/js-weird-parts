ad232847f1a920998a52947126ed0608800f625c
Tomasz Omeljaniuk <tomekomel@gmail.com>
2022-12-28 16:45:35 +0100

    feat: add methods and properties

--- greetr/app.js
+++ greetr/app.js
@@ -1,2 +1,2 @@
 var g = $G('Tomasz', 'Doe');
-console.log(g);
+g.greet().setLang('es').greet(true);
--- greetr/greetr.js
+++ greetr/greetr.js
@@ -3,7 +3,75 @@
         return new greetr.init(firstName, lastName, language);
     }
 
-    greetr.prototype = {};
+    var supportedLanguages = ['en', 'es'];
+
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola',
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos',
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion',
+    };
+
+    greetr.prototype = {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function () {
+            if (supportedLanguages.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function () {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreetings: function () {
+            return formalGreetings[this.language] + ', ' + this.fullName() + '!';
+        },
+
+        greet: function (formal) {
+            var msg;
+
+            if (formal) {
+                msg = this.formalGreetings();
+            } else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+
+        log: function () {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+
+            return this;
+        },
+
+        setLang: function (lang) {
+            this.language = lang;
+            this.validate();
+
+            return this;
+        },
+    };
 
     var init = greetr.init = function(firstName, lastName, language) {
         var self = this;
